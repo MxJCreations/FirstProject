68445115945e103bcee95f834d0851fd1451fd82
Jamie <jamiemarsh7985@gmail.com>
2021-02-04 17:30:29 +0000

    Questions set up!

--- greeting.js
+++ greeting.js
@@ -26,4 +26,35 @@ const getGreeting = () => {
                           return 'Howdy!';
     }
   };
-  console.log(getGreeting());
+  
+  const getQuestion = () => {
+      const randomNumber2 = Math.floor(Math.random() * 8);
+      switch(randomNumber2) {
+          case 0:
+              return 'How are you today?';
+              break;
+              case 1:
+                  return 'Are you feeling ok?';
+                  break;
+                  case 2:
+                      return 'Is it raining outside?';
+                      break;
+                      case 3:
+                          return 'Are you ready for your training?';
+                          break;
+                          case 4:
+                              return 'Have you had any sleep?';
+                              break;
+                              case 5:
+                                  return 'Have you called your mother lately?';
+                                  break;
+                                  case 6:
+                                      return 'Have you finished your portfolio yet?';
+                                      break;
+                                      case 7:
+                                          return 'Have you eaten anything today?';
+
+      }
+  };
+
+  
