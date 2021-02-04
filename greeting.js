59d4419f0f7b846ab6f7a4a710cd269d3350e040
Jamie <jamiemarsh7985@gmail.com>
2021-02-04 17:04:24 +0000

    Greeting established!

--- greeting.js
+++ greeting.js
@@ -1,1 +1,29 @@
-console.log('i am a legend');
+const getGreeting = () => {
+    const randomNumber = Math.floor(Math.random() * 8);
+    switch(randomNumber) {
+      case 0:
+      return 'Gday mate!';
+      break;
+      case 1:
+      return 'Hello!';
+      break;
+      case 2:
+      return 'Konnichiwa!';
+      break;
+      case 3:
+          return 'Guten tag!';
+          break;
+          case 4:
+              return 'Hola!';
+              break;
+              case 5:
+                  return 'Ciao!';
+                  break;
+                  case 6:
+                      return 'Bonjour!';
+                      break;
+                      case 7:
+                          return 'Howdy!';
+    }
+  };
+  console.log(getGreeting());
